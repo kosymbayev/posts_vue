582a0a43124035aeb7eb707d6e6d92295c4b828c
s.kos <s.kosymbayev@gmail.com>
2023-10-15 22:04:16 +0600

    Corrected detail page

--- src/pages/PostDetailPage.vue
+++ src/pages/PostDetailPage.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-        <h1>Post id = {{ $route.params.id }}</h1>
+        <h1>Post #{{ $route.params.id }}</h1>
+        <div><h3>{{ this.post.title }}</h3></div>
+        <div>{{ this.post.body }}</div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data() {
+        return {
+            post: {},
+        }
+    },
+    methods: 
+    {
+        async fetchPost(postId)// Достаю пост
+        {
+            try
+            {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params:
+                    {
+                        id: this.$route.params.id,
+                    }
+                });
+                this.post = response.data[0];
+            }
+            catch (e)
+            {
+                alert('error in fetchUsers')
+            }
+        },
+    },
+    mounted()
+    {
+        this.fetchPost();
+    },
 }
 </script>
